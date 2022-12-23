52c940edac30eb7ae273343e33773956bb72d4bc
ahmednazirmusah <ahmed999nazir@gmail.com>
2022-12-23 08:30:16 +0000

    update based on review

--- classes/student.rb
+++ classes/student.rb
@@ -6,7 +6,7 @@ class Student < Person
 
   # constructor
 
-  def initialize( age, name = 'Unkown', parent_permission: true)
+  def initialize(age, name = 'Unkown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
@@ -14,7 +14,8 @@ class Student < Person
   def play_hooky
     '¯(ツ)/¯'
   end
-  def classroom=(classroom)
+
+  def add_classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
