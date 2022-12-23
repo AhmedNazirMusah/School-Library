c1e81fedb29bb19b492b675764e094c441214846
ahmednazirmusah <ahmed999nazir@gmail.com>
2022-12-23 01:02:33 +0000

    added associations

---
+++ classes/classroom.rb
@@ -0,0 +1,15 @@
+# class Classroom
+class Classroom
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student) unless @students.include?(student)
+    student.classroom = self
+  end
+end
--- classes/nameable.rb
+++
@@ -1,6 +0,0 @@
-# class nameble
-class Nameable
-  def correct_nmae
-    raise NotImplementedError
-  end
-end
